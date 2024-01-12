8a74b184f827b45fda273f21950e2377ea807e72
abdulgits <ided.263@gmail.com>
2024-01-13 03:34:05 +0700

    asoy

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className=" flex justify-center bg-blue-600 min-h-screen items-center">
+      <div>
+        <h1> hello my name abdul </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
