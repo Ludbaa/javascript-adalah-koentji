4d7b666333e4542f5e3cb769448b7bcae1d7a403
abdulgits <ided.263@gmail.com>
2024-01-13 03:44:36 +0700

    asek

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,21 @@ function App() {
   return (
     <div className=" flex justify-center bg-blue-600 min-h-screen items-center">
       <div>
-        <h1> hello my name abdul </h1>
+        <button
+          className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+          type="submit">
+          Buy now
+        </button>
+        <button
+          className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+          type="submit">
+          Buy now
+        </button>
+        <button
+          className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+          type="submit">
+          Buy now
+        </button>
       </div>
     </div>
   );
