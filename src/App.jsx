9f4bec3ded163b1b0a9b22b7bfd063089e935735
abdulgits <ided.263@gmail.com>
2024-01-16 03:08:09 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,24 @@
 import React from 'react';
-class Button extends React.Component {
-  render () {
-    return (
-      <button
-        className="h-10  px-6 font-semibold rounded-md bg-slate-700 text-white"
-        type="submit"
-      >
-        Buy Now
-      </button>
-    );
-  }
-}
 
-function ButtonBlack () {
+const Button = props => {
   return (
     <button
-      className="h-10  px-6 font-semibold rounded-md bg-black text-white"
+      className={`h-10  px-6 font-semibold rounded-md bg-${props.variant}-700 text-black`}
       type="submit"
     >
       Buy Now
     </button>
   );
-}
+};
 
 function App () {
   return (
     <div className=" flex justify-center bg-blue-600 min-h-screen items-center">
-      <div>
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <ButtonBlack />
+      <div className="flex gap-x-4">
+
+        <Button variant="red" />
+        <Button variant="slate" />
+
       </div>
     </div>
   );
