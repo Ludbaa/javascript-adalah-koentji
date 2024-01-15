207399e60e31a75728aa654e0875d6862a01bba8
abdulgits <ided.263@gmail.com>
2024-01-16 01:58:33 +0700

    asekk

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React from 'react';
 class Button extends React.Component {
-  render() {
-    return ( <button
-      className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-      type="submit">
-      Buy now
-    </button>
+  render () {
+    return (
+      <button
+        className="h-10  px-6 font-semibold rounded-md bg-slate-700 text-white"
+        type="submit"
+      >
+        Buy Now
+      </button>
     );
   }
 }
 
-function App() {
+function ButtonBlack () {
+  return (
+    <button
+      className="h-10  px-6 font-semibold rounded-md bg-black text-white"
+      type="submit"
+    >
+      Buy Now
+    </button>
+  );
+}
+
+function App () {
   return (
     <div className=" flex justify-center bg-blue-600 min-h-screen items-center">
       <div>
-       <Button></Button>
-       <Button></Button>
-       <Button></Button>
-       <Button></Button>
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <ButtonBlack />
       </div>
     </div>
   );
