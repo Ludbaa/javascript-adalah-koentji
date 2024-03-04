0f15547c3df285afdadcec30ae873a4b57c1f5ee
abdulgits <ided.263@gmail.com>
2024-03-04 23:14:08 +0900

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Button from "./components/Elements/Button";
+import Button from './components/Elements/Button';
 
 
 function App () {
   return (
     <div className='flex justify-center min-h-screen items-center'>
       <div className='w-full max-w-xs'>
-        <h1 className='text-2xl font-semibold mb-2 text-red-500'>Login
+        <h1 className='text-2xl font-semibold mb-2 text-red-600'>Login
         </h1>
         <p className='font-medium text-slate-400 mb-7'>
           welcome, please enter your details
@@ -17,10 +17,20 @@ function App () {
               className='block text-slate-600 text-sm font-semibold  mb-2 text-left'>
               Email
             </label>
-            <input type='text' className='text-sm border rounded-4-full py-2 px-3 text-slate-500 opacity-50'
+            <input type='text' className='text-sm border rounded w-full py-2 px-3 text-slate-500 placeholder: opacity-50'
               placeholder='example@gmail.com'
             />
           </div>
+          <div className='mb-6'>
+            <label htmlFor='password'
+              className='block text-slate-600 text-sm font-semibold  mb-2 text-left'>
+              Password
+            </label>
+            <input type='password' className='text-sm border rounded w-full py-2 px-3 text-slate-500 placeholder: opacity-50'
+              placeholder='********'
+            />
+          </div>
+          <Button variant="bg-red-700 w-full">Login</Button>
         </form>
         
       </div>
--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -1,5 +1,5 @@
 const Button = (props) => {
-    const { children, variant = "bg-black" } = props;
+    const { children, variant = "bg-red-500" } = props;
     return (
       <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">
         {children}
