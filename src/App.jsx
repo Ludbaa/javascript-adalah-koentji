5bdd257a9adc1fc1e4efb538ad5d785ff11219f2
abdulgits <ided.263@gmail.com>
2024-03-04 22:56:47 +0900

    login

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,23 @@ function App () {
   return (
     <div className='flex justify-center min-h-screen items-center'>
       <div className='w-full max-w-xs'>
-        <h1 className='text-3xl font-bold mb-2 text-red-500'>Login
+        <h1 className='text-2xl font-semibold mb-2 text-red-500'>Login
         </h1>
-        <p className='font-medium text-slate-400'>
+        <p className='font-medium text-slate-400 mb-7'>
           welcome, please enter your details
         </p>
+        <form action=''>
+          <div className='mb-6'>
+            <label htmlFor='email'
+              className='block text-slate-600 text-sm font-semibold  mb-2 text-left'>
+              Email
+            </label>
+            <input type='text' className='text-sm border rounded-4-full py-2 px-3 text-slate-500 opacity-50'
+              placeholder='example@gmail.com'
+            />
+          </div>
+        </form>
+        
       </div>
     </div>
   );
